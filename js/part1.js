6cd26ee852f57a89d55a5ead9c024e64ab276d6b
Gryn Ilya <grynilia@gmail.com>
2019-02-25 17:13:44 +0200

    homework

---
+++ js/part1.js
@@ -0,0 +1,51 @@
+// Задание
+
+//1
+
+ for (let i = 0; i < 10; i++) {
+  setTimeout(function() {
+   		console.log(i);
+ 	}, 10);
+ }
+
+//2
+
+ for (var i = 0; i < 10; i++) {
+  setTimeout(function() {
+   		console.log(i);
+ 	}(i), 10);
+ }
+ 
+//3
+
+ for (var i = 0; i < 10; i++) {
+ 	(function(i){
+ 		 setTimeout(function() {
+ 		  		console.log(i);
+ 			}, 10);
+ 		}(i));
+ 
+ }
+//2 задание
+
+let result = 0;
+let maths = {
+	number: 5,
+	sum: function(){
+		result += this.number;
+    	return this;
+	},
+	minus: function(){
+    	result -= this.number;
+    	return this;
+	},
+	multiplay: function(){
+		result *= this.number;
+    	return this;
+	},
+	showResult: function(){
+      console.log(result);
+      return this;
+	}
+}
+maths.sum().sum().multiplay().minus().showResult().sum().showResult();
